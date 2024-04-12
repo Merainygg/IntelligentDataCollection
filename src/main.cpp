f4e8c5eaf9a7382a3a5a568aa213baa131428f95
Merainygg <guojunchao1@foxmail.com>
2024-04-12 18:01:13 +0800

    MSP40 Output Online Display

---
+++ src/main.cpp
@@ -0,0 +1,328 @@
+/*
+  Rui Santos
+  Complete project details
+   - Arduino IDE: https://RandomNerdTutorials.com/esp8266-nodemcu-ota-over-the-air-arduino/
+   - VS Code: https://RandomNerdTutorials.com/esp8266-nodemcu-ota-over-the-air-vs-code/
+     
+  This sketch shows a Basic example from the AsyncElegantOTA library: ESP8266_Async_Demo
+  https://github.com/ayushsharma82/AsyncElegantOTA
+*/
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>
+#include <WiFiClientSecure.h>
+// #include <HTTPClient.h>
+
+const char* ssid = "LINK_2#";
+const char* password = "link50166369";
+
+const char* servername = "http://172.20.100.40/data/";
+
+bool ledState = 0;
+const int ledPin = 2;
+int maxvalue = 0;
+int minvalue = 32768;
+int start = 0;
+int noread = 0;
+int run_count = 0;
+String sendStr = "Hello World!";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>ESP Web Server</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,">
+  <style>
+  html {
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+  }
+  h1 {
+    font-size: 1.8rem;
+    color: white;
+  }
+  h2{
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #143642;
+  }
+  .topnav {
+    overflow: hidden;
+    background-color: #143642;
+  }
+  body {
+    margin: 0;
+  }
+  .content {
+    padding: 30px;
+    max-width: 600px;
+    margin: 0 auto;
+  }
+  .card {
+    background-color: #F8F7F9;;
+    box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);
+    padding-top:10px;
+    padding-bottom:20px;
+  }
+  .button {
+    padding: 15px 50px;
+    font-size: 24px;
+    text-align: center;
+    outline: none;
+    color: #fff;
+    background-color: #0f8b8d;
+    border: none;
+    border-radius: 5px;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+   }
+   /*.button:hover {background-color: #0f8b8d}*/
+   .button:active {
+     background-color: #0f8b8d;
+     box-shadow: 2 2px #CDCDCD;
+     transform: translateY(2px);
+   }
+   .state {
+     font-size: 1.5rem;
+     color:#8c8c8c;
+     font-weight: bold;
+   }
+  </style>
+<title>ESP Web Server</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="data:,">
+</head>
+<body>
+  <div class="topnav">
+    <h1>ESP WebSocket Server</h1>
+  </div>
+  <div class="content">
+    <div class="card">
+      <h2>MSP40 Output Online Display</h2>
+      <!-- <p>state: <span id="state">%STATE%</span></p> 
+      <p class="state">state: <span id="state">%STATE%</span></p>
+      <p><button id="button" class="button">Toggle</button></p>
+      -->
+      <p><span id="text">%TEXT%</span></p>
+    </div>
+  </div>
+<script>
+  var gateway = `ws://${window.location.hostname}/ws`;
+  var websocket;
+  window.addEventListener('load', onLoad);
+  function initWebSocket() {
+    console.log('Trying to open a WebSocket connection...');
+    websocket = new WebSocket(gateway);
+    websocket.onopen    = onOpen;
+    websocket.onclose   = onClose;
+    websocket.onmessage = onMessage; // <-- add this line
+  }
+  function onOpen(event) {
+    console.log('Connection opened');
+  }
+  function onClose(event) {
+    console.log('Connection closed');
+    setTimeout(initWebSocket, 2000);
+  }
+  function onMessage(event) {
+    var state;
+    if (event.data == "1"){
+      state = "ON";
+    }
+    else{
+      state = "OFF";
+    }
+    document.getElementById('state').innerHTML = state;
+  }
+  function onLoad(event) {
+    initWebSocket();
+    initButton();
+  }
+  function initButton() {
+    document.getElementById('button').addEventListener('click', toggle);
+  }
+  function toggle(){
+    websocket.send('toggle');
+  }
+  setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var text = this.responseText;
+      document.getElementById("text").innerHTML = text;
+
+      var maxvalue = text.split(",")[2].split(":")[1];
+      var minvalue = text.split(",")[3].split(":")[1];
+      var run_count = text.split(",")[1].split(":")[1];
+
+      var xmlhttp = new XMLHttpRequest();
+      var url = "http://172.20.100.40:8081/data";
+      xmlhttp.open("POST", url, true);
+      xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xmlhttp.send("maxvalue="+maxvalue+"&minvalue="+minvalue+"&run_count="+run_count);
+
+      }
+  };
+  xhttp.open("GET", "/state", true);
+  xhttp.send();
+
+}, 300);
+
+</script>
+</body>
+</html>)rawliteral";
+
+void notifyClients() {
+  ws.textAll(String(ledState));
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    if (strcmp((char*)data, "toggle") == 0) {
+      ledState = !ledState;
+      notifyClients();
+    }
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+             void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+String processor(const String& var){
+  Serial.println(var);
+  if(var == "STATE"){
+    if (ledState){
+      return "ON";
+    }
+    else{
+      return "OFF";
+    }
+  }
+  return String();
+}
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(57600);
+
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
+
+  initWebSocket();
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+ server.on("/state", HTTP_GET, [](AsyncWebServerRequest *request){
+  request->send_P(200, "text/plain", sendStr.c_str());
+  });
+  // Start ElegantOTA
+  AsyncElegantOTA.begin(&server);
+  // Start server
+  server.begin();
+}
+
+void loop() {
+  // ws.cleanupClients();
+//   digitalWrite(ledPin, ledState);0
+  int count = 0;
+  int arr[] = {1, 2, 3, 4, 5, 6, 7};
+  while (count < 7)
+  {
+    if (Serial.available() > 0)
+    {
+      noread = 0;
+    }
+    else
+    {
+      noread++;
+    }
+    delay(1);
+    for (int i = 0; i < Serial.available(); i++) {
+      arr[count] = Serial.read();
+      if(arr[0] == 0xF0)
+        count++;
+      else
+        count = 0;
+      
+      if (count == 7)
+        break;    
+      
+
+    }
+    if (noread > 20) {
+      run_count = 0;
+      start = 0;
+      break;
+    }
+
+  }
+      if (start < 10 || noread > 20) {
+        // break;
+        maxvalue = 0;
+        minvalue = 32768;
+        sendStr = "No data received";
+      }
+      else{
+
+        int index = arr[3];
+        int value = arr[4]*256+ arr[5];
+        if (value > maxvalue) {
+          maxvalue = value;
+        }
+        if (value < minvalue) {
+          minvalue = value;
+        }
+        run_count++;
+        // HTTPC
+        // sendStr = String(arr[0])+String(arr[1])+String(arr[2])+String(arr[3])+String(arr[4])+String(arr[5])+String(arr[6]);
+        sendStr = "Testing,count:"+String(run_count)+",max:"+String(maxvalue)+",min:"+String(minvalue) + ",delta:"+String(maxvalue-minvalue);
+      }
+  start++;
+}
